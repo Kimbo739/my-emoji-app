b3aaa75f1e50f24eef8eb6984da8e6e4579b0324
Kimbo739 <kimviolet@gmail.com>
2020-12-04 21:28:21 -0500

    Deleted an emoji

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myEmojis = ["👨‍💻", "⛷", "🍲", "💋", "🐸"]
+const myEmojis = ["👨‍💻", "🍲", "💋", "🐸"]
 
 function renderEmojis() {
     const emojiContainer = document.getElementById("emoji-container")
